2f202ff217a3d5c5d3a6653290ef53fcd82de064
jefflin <linjef0711@gmail.com>
2020-12-31 13:04:38 +0800

    feat: add test page

--- app.js
+++ app.js
@@ -3,25 +3,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
 const logGenerator = require('./logGenerator')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.use(logGenerator)
 
-
 app.get('/', (req, res) => {
-    res.send('列出全部 Todo')
+    res.render('index')
 })
 
 app.get('/new', (req, res) => {
-    res.send('新增 Todo 頁面')
+    res.send('<h2>新增 Todo 頁面</h2><button><a href="/">返回列出全部 Todo</button></a>')
 })
 
 app.get('/:id', (req, res) => {
-    res.send('顯示一筆 Todo')
+    res.send('<h2>顯示一筆 Todo</h2><button><a href="/">返回列出全部 Todo</a></button>')
 })
 
 app.post('/', (req, res) => {
-    res.send('新增一筆  Todo')
+    res.send('<h2>新增一筆  Todo</h2><button><a href="/">返回列出全部 Todo</a></button>')
 })
 
 app.listen(port, () => {
