2edaf9d23dee4ca9d3ce67f9e8f5a691aba856be
jefflin <linjef0711@gmail.com>
2021-01-01 11:08:13 +0800

    feat: add request and response`s time stamps

--- logGenerator.js
+++ logGenerator.js
@@ -7,7 +7,12 @@ function logGenerator(req, res, next) {
     res.on('finish', () => {
         const resTime = new Date()
         const timeDiff = resTime - reqTime
+        const reqstamps = `request: ${reqTime.toLocaleString()}:${reqTime.getMilliseconds()}`
+        const resstamps = `response: ${resTime.toLocaleString()}:${resTime.getMilliseconds()}`
         const stamps = `${reqTime.toLocaleString()} | ${method} from ${URL} | total time: ${timeDiff} ms`
+
+        console.log(reqstamps)
+        console.log(resstamps)
         console.log(stamps)
     })
 
